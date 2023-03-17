ae85b8317998718caf4c67d06b8b46a7ae837e80
Timothée Bermond <94856338+Timothee-Bermond@users.noreply.github.com>
2023-03-17 18:55:33 +0100

    Update rejoindre_partie.js

--- static/rejoindre_partie.js
+++ static/rejoindre_partie.js
@@ -1,14 +1,17 @@
 document.getElementById("bouton_rejoindre").addEventListener("click", () => {
-    fetch('/rejoindre',{
-        method:'POST',
-        credentials: 'include',
-        headers:{
-          'Content-Type': 'application/json',
-        },
-        body:JSON.stringify(document.getElementById("input_id_partie").textContent)
-      })
-      .then(
-        window.location.replace('./yams.html')
-        )
+  info = {
+    id_partie:document.getElementById("input_id_partie").value
+  }
+  fetch('/rejoindre',{
+      method:'POST',
+      credentials: 'include',
+      headers:{
+        'Content-Type': 'application/json',
+      },
+      body:JSON.stringify(info)
+    })
+    .then(
+      window.location.replace('./yams.html')
+      )
     
-  })
+})
